7e7fc0539d470000e478c369249a6391d46593b9
Marco Bruno <marcobrunolobovero@gmail.com>
2024-08-03 13:26:53 -0600

    login terminado
    se termina el login y se le agregan los estilos. tambien se finaliza el apartado de ofertas

--- Views/login.php
+++ Views/login.php
@@ -10,8 +10,36 @@
     <link rel="stylesheet" href="./Assets/css/login.css">
 </head>
 <body>
-    <header>
-        
+<header>
+        <nav class="navbar"class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="./index">Logo</a>
+                <button class="navbar-toggler" type="button"
+                    data-bs-toggle="collapse" data-bs-target="#navbarText"
+                    aria-controls="navbarText" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Voluntarios</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Comercios afiliados</a>
+                        </li>
+                        <li class="nav-item">
+                            <span>
+                                <a class="nav-button" href="#patrocinadores">Patrocinadores/afiliados</a>
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     </header>
     <section id="myForm" class="mt-3">
         <h1 class="text-center">Log-in</h1>
