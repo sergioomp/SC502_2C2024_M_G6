57b58e00cb0738df1e8768afce367459bb676693
Wolfblackcr <oscarrosales899@gmail.com>
2024-08-06 00:19:01 -0600

    arreglo de formularios y implantación de js

---
+++ Views/Assets/js/CPatrocinador.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('#formulario').on('submit', function (e) {
+        e.preventDefault();
+        var formData = new FormData($('#formulario')[0])
+        $.ajax({
+            url: '../controller/ControllerPatrocinador.php',
+            type: 'POST',
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function (response) {
+                $('#response').html('<div class="alert alert-success">Denuncia enviada exitosamente!</div>');
+            },
+            error: function (err) {
+
+                $('#response').html('<div class="alert alert-danger">Error al denunciar.</div>');
+            }
+        });
+    })
+});
---
+++ Views/Assets/js/FromTerminos.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const checkbox = document.getElementById('gridCheck');
+  const button = document.getElementById('registerButton');
+
+  checkbox.addEventListener('change', function () {
+    if (this.checked) {
+      button.removeAttribute('disabled');
+    } else {
+      button.setAttribute('disabled', 'true');
+    }
+  });
+});
+
+
+function showTerms() {
+  document.getElementById('terms').style.display = 'block';
+}
+
+function closeTerms() {
+  document.getElementById('terms').style.display = 'none';
+}
+
+document.getElementById('acceptTerms').addEventListener('change', function() {
+  document.getElementById('registerButton').disabled = !this.checked;
+});
+
+function register() {
+  window.location.href = './index.html';
+}
+
+
+
